d45dae9cdfa3d0dfb8c4ed5022891bebb5eef2d4
Rohan Sakpal <rohan.sakpal@atriina.com>
2024-10-04 08:08:51 +0530

    Rohan:Login 1Day expire

--- Client/src/app/auth/guard/auth.guard.ts
+++ Client/src/app/auth/guard/auth.guard.ts
@@ -9,5 +9,25 @@ export const authGuard: CanActivateFn = (route, state) => {
     router.navigate(["/login"]);
     return false;
   }
+
+  // Check session expiry
+  const loginTime = localStorage.getItem('LoginTime');
+  if (loginTime) {
+    const currentTime = new Date().getTime();
+    const timeDifference = currentTime - Number(loginTime);
+    const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+
+    // If session is older than 24 hours, force logout and redirect to login
+    if (timeDifference > oneDayInMilliseconds) {
+      msalService.logout(); // Optional: You can also clear localStorage here
+      router.navigate(["/login"]);
+      return false;
+    }
+  } else {
+    // If no login time is found, redirect to login
+    router.navigate(["/login"]);
+    return false;
+  }
+  
   return true;
 };
