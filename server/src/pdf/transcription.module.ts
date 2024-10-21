a2e5289b12d557720f330a24173fe66400ce39ce
ADMIN <90824857+jadhavakanksha@users.noreply.github.com>
2024-10-21 14:01:36 +0530

    add pdf functionality for chat and summary

--- server/src/pdf/transcription.module.ts
+++ server/src/pdf/transcription.module.ts
@@ -1,22 +1,31 @@
-import { TranscriptionEntity } from './transcription.entity';
 import { Module } from '@nestjs/common';
 import { PdfService } from './pdf.service';
 import { TranscriptionService } from './transcription.service';
 import { TranscriptionController } from './transcription.controller';
 import { AzureCosmosDbModule } from '@nestjs/azure-database';
-
+import { TranscriptionEntity } from './transcription.entity';
+import { ProjectEntity } from 'src/audio/entity/project.entity';
+import { TargetGroupEntity } from 'src/audio/entity/target.entity';
 
 
 @Module({
-    imports:[AzureCosmosDbModule.forFeature([
-        {
-        collection:'Transcription',
-        dto: TranscriptionEntity
-        }
-
-])],
+    imports: [
+        AzureCosmosDbModule.forFeature([
+            {
+                collection: 'Transcription',
+                dto: TranscriptionEntity,
+            },
+            {
+                collection: 'Projects', // Ensure this matches the name of your collection
+                dto: ProjectEntity,
+            },
+            {
+                collection: 'TargetGroups', // Ensure this matches the name of your collection
+                dto: TargetGroupEntity,
+            },
+        ]),
+    ],
     controllers: [TranscriptionController],
-    providers:[PdfService,TranscriptionService]
+    providers: [PdfService, TranscriptionService],
 })
 export class TranscriptionModule {}
-
